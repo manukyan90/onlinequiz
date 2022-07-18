f1cbcaa5ac0c224ce2e6c8bcdd74df800791522f
Andranik <andranik.manukyanr@gmail.com>
2022-07-18 19:15:44 +0400

    HTML JS SCC

---
+++ HTML/front.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>LOTTO BIG WIN</title>
+        <link rel="stylesheet" href="../CSS/style.css">
+
+    </head>
+    <body>
+    <h1>LOTTO BIG WIN</h1>
+        <table id="elem" align="center">
+            <tr>
+                <td class="balls" id="00"></td>
+                <td class="balls" id="01"></td>
+                <td class="balls" id="02"></td>
+                <td class="balls" id="03"></td>
+                <td class="balls" id="04"></td>
+                <td class="balls" id="05"></td>
+                <td class="balls" id="06"></td>
+                <td class="balls" id="07"></td>
+                <td class="balls" id="08"></td>
+                <td class="balls" id="09"></td>
+            </tr>
+            <tr>
+                <td class="balls" id="10"></td>
+                <td class="balls" id="11"></td>
+                <td class="balls" id="12"></td>
+                <td class="balls" id="13"></td>
+                <td class="balls" id="14"></td>
+                <td class="balls" id="15"></td>
+                <td class="balls" id="16"></td>
+                <td class="balls" id="17"></td>
+                <td class="balls" id="18"></td>
+                <td class="balls" id="19"></td>
+            </tr>
+            <tr>
+                <td class="balls" id="20"></td>
+                <td class="balls" id="21"></td>
+                <td class="balls" id="22"></td>
+                <td class="balls" id="23"></td>
+                <td class="balls" id="24"></td>
+                <td class="balls" id="25"></td>
+                <td class="balls" id="26"></td>
+                <td class="balls" id="27"></td>
+                <td class="balls" id="28"></td>
+                <td class="balls" id="29"></td>
+            </tr>
+        </table>
+            <button type="submit" id="but" onclick="generate()">Play</button>
+            <button id="but1">Get</button> <!--heto uxxel ID-n-->
+
+
+
+        <script src = "../JS/lotto.js"></script>
+
+
+    </body>
+</html>
