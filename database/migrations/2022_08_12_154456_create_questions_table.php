1ea746e954cb61568869f02d31c84ac2601cda64
Andranik <andranik.manukyanr@gmail.com>
2022-08-12 20:13:45 +0400

    question table

---
+++ database/migrations/2022_08_12_154456_create_questions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->text("title");
+            $table->unsignedBigInteger('subject_id')->index();
+            $table->foreign('id')->on('subject')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+};
